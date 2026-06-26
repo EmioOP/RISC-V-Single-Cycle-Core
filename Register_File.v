f2c763a675690547f5a8f9173828873bd791b7b4
Deepu K <deepuemio@gmail.com>
2026-06-26 17:46:26 +0530

    refactor: rename module to Register_File and standardize reset logic in output assignments

--- Register_File.v
+++ Register_File.v
@@ -1,21 +1,27 @@
-module regFile (
-    A1,A2,A3,clk,WE3,rst,WD3,RD1,RD2
-);
+module Register_File (A1,A2,A3,clk,WE3,rst,WD3,RD1,RD2);
 
 input [31:0] WD3;
 input [4:0] A1,A2,A3;
-input clk,rst;
+input clk,rst,WE3;
+
+output [31:0] RD1,RD2;
 
 reg [31:0] Registers[31:0];
-
-
-assign RD1 = (!rst) ? 32'h00000000 : Registers[A1];
-assign RD2 = (!rst) ? 32'h00000000 : Registers[A2];
 
 always @(posedge clk) begin
     if(WE3)begin
         Registers[A3] <= WD3;
     end
 end
+
+
+assign RD1 = (~rst) ? 32'd0 : Registers[A1];
+assign RD2 = (~rst) ? 32'd0 : Registers[A2];
+
+initial begin
+    Registers[9] = 32'h00000020;
+end
+
+
     
 endmodule
