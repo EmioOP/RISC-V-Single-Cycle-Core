a1e7d3d3240eef1ad7506ec4e9f4e5bb5e055cc0
Deepu K <deepuemio@gmail.com>
2026-06-26 17:46:37 +0530

    refactor: update sign extension logic for clarity and consistency

--- Sign_Extend.v
+++ Sign_Extend.v
@@ -3,6 +3,6 @@ module Sign_Extend(In,Imm_Ext);
     input [31:0] In;
     output [31:0] Imm_Ext;
 
-    assign Imm_Ext = (In[31]) ? {20{1'b1},In[31:20]} : {20{1'b0},In[31:20]};
-
+    // assign Imm_Ext = (In[31]) ? {20'b11111111111111111111,In[31:20]} : {20'b00000000000000000000,In[31:20]};
+    assign Imm_Ext = (In[31]) ? {{20{1'b1}},In[31:20]} : {{20{1'b0}},In[31:20]};
 endmodule
